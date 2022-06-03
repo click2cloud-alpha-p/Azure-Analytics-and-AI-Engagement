da3f54669928b333b7b4941922ff33e06f2b4f7e
click2cloud-SanchitG <86832286+click2cloud-SanchitG@users.noreply.github.com>
2022-06-03 12:23:14 +0530

    Update prerequisite_script.ps1

--- Manufacturing/automation/prerequisite_script.ps1
+++ Manufacturing/automation/prerequisite_script.ps1
@@ -1,3 +1,6 @@
+# Install Nuget Package Manager
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
 # Install azure cli tool i.e. az
 Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
 
